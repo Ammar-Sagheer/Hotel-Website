5ddf167a0a05e15695254d8eb4ccbf41aff48e47
Ammar <ammarsagheer@gmail.com>
2026-06-18 23:17:04 +0500

    Before implementing Editing reservation

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -1,13 +1,13 @@
 "use server";
 
+import { revalidatePath } from "next/cache";
 import { auth, signIn, signOut } from "./auth";
 import { supabase } from "./supabase";
+import { getBookings } from "./data-service";
 
 export async function updateGuest(formData) {
   const session = await auth();
-
   if (!session) throw new Error("You must be logged in");
-
   const nationalID = formData.get("nationalID");
   const [nationality, countryFlag] = formData.get("nationality").split("%");
   const nationalIDRegex = /^[a-zA-Z0-9]{6,12}$/;
@@ -21,6 +21,25 @@ export async function updateGuest(formData) {
     .eq("id", session.user.guestId);
 
   if (error) throw new Error("Guest could not be updated");
+  revalidatePath("/account/profile");
+}
+
+export async function deleteReservation(bookingId) {
+  const session = await auth();
+  if (!session) throw new Error("You must be logged in");
+
+  const guestBookings = await getBookings(session.user.guestId);
+  const guestBookingIds = guestBookings.map((booking) => booking.id);
+  if (!guestBookingIds.includes(bookingId))
+    throw new Error("You are not allowed to delete this bookingf");
+
+  const { error } = await supabase
+    .from("bookings")
+    .delete()
+    .eq("id", bookingId);
+
+  if (error) throw new Error("Booking could not be deleted");
+  revalidatePath("/account/reservations");
 }
 
 export async function signInAction() {
